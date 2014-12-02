ec8072b958b1cca50410beb23b78166c35c7b5aa
Erik Espana <espanae@union.edu>
2014-12-02 12:15:29 -0500

    Comments added

--- php/working-with-templates/publish-template/auth_user.php
+++ php/working-with-templates/publish-template/auth_user.php
@@ -1,5 +1,8 @@
 <?php
+// Include Wing's web services library:
 require_once( '/your/path/cascade_ws/ws_lib.php' );
+// Download the library from:
+// http://www.upstate.edu/cascade-admin/projects/web-services/oop/classes/index.php
 
 $wsdl = "https://cascade.domain.edu:port/ws/services/AssetOperationService?wsdl";
 $auth           = new stdClass();
