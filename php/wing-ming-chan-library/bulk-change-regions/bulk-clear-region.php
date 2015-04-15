4066ec45c8bd3f13fc877888bb1f49ae3d66dacd
Erik Espana <espanae@union.edu>
2015-04-15 15:19:49 -0400

    Try passing in two variables and NULL

--- php/wing-ming-chan-library/bulk-change-regions/bulk-clear-region.php
+++ php/wing-ming-chan-library/bulk-change-regions/bulk-clear-region.php
@@ -1,29 +1,29 @@
 <?php
+// Source variables
 $site_name   = "reboot";
 $page_name   = "/_Site Support/Banks/Banners/Academics Bank";
-
 $output = "PHP";
 $region1 = "NAVIGATIONANCESTORS";
 $region2 = "NAVIGATIONCONTEXTUAL";
 ?>
 <h1>Attach Block to Page Region</h1>
 <pre>
-    <?php
+<?php
+
 try
 {
     require_once('auth_espanae_dev.php');
-    echo "require_once\n";
     $page        = $cascade->getAsset( Page::TYPE, $page_name, $site_name );
-    echo "\$page = \$page\n";
+    echo "\$page: \$page\n";
     
-    $page->
-        setRegionBlock(
-            $output,
-            $region1,
-            $cascade->getAsset( 
-                Block::TYPE, '3dcb4d6b956aa05200c85bbb924fd235' ))->
-        edit();
-    echo "\$page->setRegionBlock()\n";
+    // Try passing in a NULL
+    $page->setRegionBlock( $output, $region1, NULL)->edit();
+    echo $region1." block: Tried passing in a NULL\n";
+
+    // Try just passing in two, not three, arguments, leaving the last one empty.
+    $page->setRegionBlock( $output, $region2)->edit();
+    echo $region2." block: Tried passing in a NULL\n";
+
 }
 catch( Exception $e )
 {
