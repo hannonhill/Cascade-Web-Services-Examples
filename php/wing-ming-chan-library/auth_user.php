425cc9d7a7278362bb2a82f5e226fe81daa53b74
Erik Espana <espanae@users.noreply.github.com>
2015-06-19 09:50:37 -0400

    Comments added

--- php/wing-ming-chan-library/auth_user.php
+++ php/wing-ming-chan-library/auth_user.php
@@ -1,16 +1,20 @@
 <?php
-// Include local copy of Wing's web services library
+// Download the web services library here
 // http://www.upstate.edu/cascade-admin/projects/web-services/oop/classes/
+
+// Point to the downloaded library
 require_once( '/your/path/cascade_ws/ws_lib.php' );
 
+// Set the AssetOperationService WSDL URL, the entry point to all available Cascade web service operations. 
 $wsdl = "https://cascade.domain.edu:port/ws/services/AssetOperationService?wsdl";
 $auth           = new stdClass();
 $auth->username = "my_username"; /* change this */
 $auth->password = "my_password"; /* change this */
 
 try {
-  // set up the service
+  // Initialize the $service object so it can be referenced in the calling script.
   $service = new AssetOperationHandlerService( $wsdl, $auth );
+  // 
   $cascade = new Cascade( $service );
 
   // create an empty object for a one-time operation
